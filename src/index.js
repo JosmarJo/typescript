01920770652de3bb04f4573a6a6d1fc67be48511
JosmarJO <yomadgil@gmail.com>
2024-09-03 13:37:01 -0400

    condicionales, operaciones aritmeticas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log("hola mundo");
+//tipos de variables
+var variable_local;
+var variable_global;
+var pi = 3.14;
+var texto = "textos";
+var numeros = 0;
+var booleano = true;
+//condicionales
+var edad = 18;
+if (edad < 18) {
+    console.log("eres menor de edad");
+}
+else {
+    console.log("eres mayor de edad");
+}
+//operaciones aritmeticas
+var suma = 5 + 5;
+var resta = 3 - 2;
+var multiplicacion = 5 * 2;
+var divison = 4 / 2;
+var modulo = 5 % 5;
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,21 @@ let variable_local
 var variable_global
 const pi = 3.14
 
-let texto:string
-let numeros:number
-let booleano:boolean
+let texto:string="textos"
+let numeros:number=0
+let booleano:boolean=true
+
+//condicionales
+let edad:number=18
+if (edad < 18){
+    console.log("eres menor de edad")
+}else{
+    console.log("eres mayor de edad")
+}
+
+//operaciones aritmeticas
+let suma: number= 5 + 5
+let resta: number= 3 - 2
+let multiplicacion: number= 5 * 2
+let divison: number= 4 / 2
+let modulo: number= 5 % 5
